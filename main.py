062095f273294e1069db4c3b597812d1b7f24757
TyaminAlexey <51873876+TyaminAlexey@users.noreply.github.com>
2021-03-23 00:30:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#Creating telegtambot.herokuapp.com... done
+#HEROKU_OAUTH_ID=09eb2f3e-c3b4-4db6-974c-395cb83b6c5f
+#HEROKU_OAUTH_SECRET=5b2c10e6-5e7c-4b3c-8607-71282a5c298d
+
+
+#heroku config:set -a telegtambot HEROKU_OAUTH_ID=09eb2f3e-c3b4-4db6-974c-395cb83b6c5f HEROKU_OAUTH_SECRET=5b2c10e6-5e7c-4b3c-8607-71282a5c298d
