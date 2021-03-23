863aa8427fed1569c3a58931fc0ac9d303aedf70
TyaminAlexey <51873876+TyaminAlexey@users.noreply.github.com>
2021-03-23 22:14:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,26 @@ import os
 server = flask.Flask(__name__)
  
  
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    bot.process_new_updates([types.Update.de_json(
-         flask.request.stream.read().decode("utf-8"))])
-    return "!", 200
- 
- 
-@server.route('/', methods=["GET"])
-def index():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
-    return "Hello from Heroku!", 200
+import json
+import requests
+i = 1
+def handler(event, context):
+    body = json.loads(event['body'])
+    global i
+    i += 2
+    return {
+        
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps({
+            
+            'text': i                       
+            
+        }),
+        'isBase64Encoded': False
+    }
  
  
 if __name__ == "__main__":
